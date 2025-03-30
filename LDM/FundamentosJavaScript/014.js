e118f08e9696b30b85427988727565b559514cd6
Ruben <ojedafj@alumnos.ilerna.com>
2025-03-31 00:23:14 +0200

    Commit Fundamentos de JavaScript 5

---
+++ LDM/FundamentosJavaScript/014.js
@@ -0,0 +1,9 @@
+let result;
+let a = 3;
+let b = 0;
+
+result = a + b < 4? "Baja la ropa sucia": "Sube los zapatos";
+
+alert(result);
+
+//Duda : Si no le doy valores a a o b no me salta el alert???
